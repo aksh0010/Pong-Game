fe24e813c1e2445ef62ae9b83c190c353d6a0854
aksh0010 <akshpatelofficial@gmail>
2024-03-01 16:01:11 -0500

    Adding files

---
+++ src/comp2800_lab4/SoundPlayer.java
@@ -0,0 +1,26 @@
+package comp2800_lab4;
+
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import java.io.File;
+/*Creating SoundPlayer class which opens a file
+ * reads the .wav format audio
+ * and then plays it
+ * */
+public class SoundPlayer {
+    public static void playSound(String filename) {
+        try {
+            AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(new File(filename));
+            Clip clip = AudioSystem.getClip();
+            clip.open(audioInputStream);
+            clip.start();
+            
+            
+//            Thread.sleep(clip.getMicrosecondLength() / 1000);
+        } catch (Exception ex) {
+            System.out.println("Error with playing sound.");
+            ex.printStackTrace();
+        }
+    }
+}
